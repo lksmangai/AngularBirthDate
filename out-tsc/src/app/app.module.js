988cf2b189bbd979f9a42e8d72118a0851bad485
Sankaralingam Lakshmanaraj <lksmangai@yahoo.com>
2020-02-22 17:52:20 +0530

    Auto-generated commit

---
+++ out-tsc/src/app/app.module.js
@@ -0,0 +1,27 @@
+import { __decorate } from "tslib";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './material.module';
+let AppModule = class AppModule {
+};
+AppModule = __decorate([
+    NgModule({
+        declarations: [
+            AppComponent
+        ],
+        imports: [
+            BrowserModule,
+            AppRoutingModule,
+            FormsModule,
+            ReactiveFormsModule,
+            MaterialModule
+        ],
+        providers: [],
+        bootstrap: [AppComponent]
+    })
+], AppModule);
+export { AppModule };
+//# sourceMappingURL=app.module.js.map
